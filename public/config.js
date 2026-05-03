5550b6972e777f00f6c6b80ebd7ad194ad0169eb
Abdul Samad <iamabdulsamad2.0@gmail.com>
2026-05-03 16:50:58 +0500

    fixed the client id "

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 export const OIDC_CONFIG = {
-  clientId: "a7030612554329ed705faf29e9ec3855",
+  clientId: "f797cf821cf171a3e42d5c37e6b5bee1",
   redirectUri: window.location.origin + "/",
   authorizeEndpoint: "https://my-oidc.vercel.app/o/authorize",
   userInfoEndpoint: "https://my-oidc.vercel.app/o/userinfo",
